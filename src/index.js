4c5018044784a33b9ac068315aceeaa7c2ef70fc
tataqt <tata_98@ukr.net>
2020-07-25 19:07:28 +0300

    add component template output

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './style/index.scss';
 import { Excel } from '@/components/excel/Excel';
+import { Header } from '@/components/header/Header';
+import { Toolbar } from '@/components/toolbar/toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Table } from '@/components/table/Table';
 
 const excel = new Excel('#app', {
-    components: [
-
-    ],
+    components: [Header, Toolbar, Formula, Table],
 });
 
-console.log("Excel", excel);
+excel.render();
