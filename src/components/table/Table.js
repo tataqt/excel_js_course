61959ec460b851100e4bf2516a6e0b1cddfa3c22
tataqt <tata_98@ukr.net>
2020-08-14 17:07:26 +0300

    add base event

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,36 @@
-import { ExcelComponent } from "@core/ExcelComponent";
-import { createTable } from "./table.template";
+import {
+    ExcelComponent
+} from "@core/ExcelComponent";
+import {
+    createTable
+} from "./table.template";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
 
+    constructor($root) {
+        super($root, {
+            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        })
+    }
+
     toHTML() {
         return createTable(20)
+    }
+
+    onClick(event) {
+        console.log('click', event.target);
+    }
+
+    onMousedown() {
+        console.log('mousedown');
+    }
+
+    onMousemove() {
+        console.log('mousemove');
+    }
+
+    onMouseup() {
+        console.log('mouseup');
     }
 }
